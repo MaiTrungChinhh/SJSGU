3a0ab82f3da2050427c012ff15db2a726ec011e6
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2025-02-24 13:18:36 +0700

    Create PJ

---
+++ cache/t_compile/46fa2a4d1c2dfdfb7e296f4a3f7227463c634e62^e9be0ecfd9cc66bba6b2c896d74703d29644ffc5_0.app.frontendcomponentsnavigationMenusdashboardMenuItem.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 4.3.1, created on 2025-02-24 06:13:42
+  from 'app:frontendcomponentsnavigationMenusdashboardMenuItem.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_67bc0e16702115_79526745',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e9be0ecfd9cc66bba6b2c896d74703d29644ffc5' => 
+    array (
+      0 => 'app:frontendcomponentsnavigationMenusdashboardMenuItem.tpl',
+      1 => 1732910728,
+      2 => 'app',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67bc0e16702115_79526745 (Smarty_Internal_Template $_smarty_tpl) {
+echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'escape' ][ 0 ], array( $_smarty_tpl->tpl_vars['navigationMenuItem']->value->getLocalizedTitle() ));?>
+
+<span class="task_count">
+	<?php echo $_smarty_tpl->tpl_vars['unreadNotificationCount']->value;?>
+
+</span>
+<?php }
+}
